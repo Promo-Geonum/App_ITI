16a4df7d4a81f18e62961788c7e60031afe853e0
IdrissaD <73114156+IdrissaD@users.noreply.github.com>
2021-03-04 14:31:43 +0100

    Lien avec boutons

--- app/Carte/leafletmap.js
+++ app/Carte/leafletmap.js
@@ -85,3 +85,87 @@ var geocoder = L.Control.geocoder({
 	new L.marker([y, x]).addTo(mymap)   
   })
 geocoder.addTo(mymap);
+
+
+
+//GESTION DONNEES PRECEDENTES
+console.log('Saison :', localStorage.saison)
+console.log('Milieu :', localStorage.milieu)
+console.log('Sport :', localStorage.sport)
+
+etape = JSON.parse(localStorage.getItem('etape'))
+console.log('Etape :', etape)
+
+new L.marker([etape.geometry.coordinates[1], etape.geometry.coordinates[0]]).addTo(mymap)
+
+var selectedPoint = null;
+
+// empty geojson layer for the shortes path result
+var pathLayer = L.geoJSON(null);
+
+// draggable marker for starting point. Note the marker is initialized with an initial starting position
+var sourceMarker = L.marker([43.716484655457045 , 7.257113456726074], {
+	draggable: true
+})
+	.on("dragend", function(e) {
+		selectedPoint = e.target.getLatLng();
+		getVertex(selectedPoint);
+		getRoute();
+	})
+	.addTo(map);
+
+// draggbale marker for destination point.Note the marker is initialized with an initial destination positon
+var targetMarker = L.marker([43.71330136382882, 7.26245641708374], {
+	draggable: true
+})
+	.on("dragend", function(e) {
+		selectedPoint = e.target.getLatLng();
+		getVertex(selectedPoint);
+		getRoute();
+	})
+	.addTo(map);
+
+
+// function to get nearest vertex to the passed point
+function getVertex(selectedPoint) {
+	var url = `http://localhost:8080/geoserver/projetgeonum/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=projetgeonum%3Anoeud&maxFeatures=50&outputFormat=application%2Fjson&viewparams=x:${
+		selectedPoint.lng
+	};y:${selectedPoint.lat};`;
+	$.ajax({
+		url: url,
+		async: false,
+		success: function(data) {
+			loadVertex(
+				data,
+				selectedPoint.toString() === sourceMarker.getLatLng().toString()
+			);
+		}
+	});
+}
+
+
+// function to update the source and target nodes as returned from geoserver for later querying
+function loadVertex(response, isSource) {
+	var features = response.features;
+	mymap.removeLayer(pathLayer);
+	if (isSource) {
+		source = features[0].properties.id;
+	} else {
+		target = features[0].properties.id;
+	}
+}
+
+// function to get the shortest path from the give source and target nodes
+function getRoute() {
+	var url = `http://localhost:8080/geoserver/projetgeonum/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=projetgeonum%3Achemin&maxFeatures=50&outputFormat=application%2Fjson&viewparams=source:${source};target:${target};`;
+
+	$.getJSON(url, function(data) {
+		mymap.removeLayer(pathLayer);
+		pathLayer = L.geoJSON(data);
+		mymap.addLayer(pathLayer);
+	});
+}
+
+getVertex(sourceMarker.getLatLng());
+getVertex(targetMarker.getLatLng());
+getRoute();
